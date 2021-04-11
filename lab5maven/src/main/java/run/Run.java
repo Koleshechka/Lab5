d38ac9c6d0e4f8de54be36f0dca5fecda468847a
Svetlana <Koleshechka@mail.ru>
2021-04-11 15:07:12 +0300

    Change main method

--- lab5maven/src/main/java/run/Run.java
+++ lab5maven/src/main/java/run/Run.java
@@ -6,9 +6,10 @@ import tools.*;
 
 public class Run {
     public static void main(String[] args) {
-        String path = "/Users/svetlana/Documents/GitHub/Lab5/lab5maven/src/main/java/input.jso";
+        String path ="";
+        //path = "/Users/svetlana/Documents/GitHub/Lab5/lab5maven/src/main/java/input.jso";
         try {
-            //String path = args[0];
+            path = args[0];
         }catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Аргумент - имя файла не найден.");
         }
